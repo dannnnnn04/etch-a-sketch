fc3c771468f61e19cfc4f72c316ad20c14337a83
Daniel_J Carig <24857822+dannnnnn04@users.noreply.github.com>
2023-12-23 14:12:24 -0800

    Add function to update # of squares for the grid
    -moving the slider will update the number of squares per side for the new grid
    -limit the slider input to 100 since a large number of squares could potentially cause delays, freezing, or crashing

--- script.js
+++ script.js
@@ -13,12 +13,27 @@ for (let i = 0; i < 256; i++) {
 
 createGrid();
 
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 const slider = document.querySelector('#slider')
 const screenVal = document.querySelector('.value');
 slider.addEventListener('input', function(){
     let val = document.getElementById('slider').value;
     screenVal.textContent = `${val} x ${val}`;
-
+    removeAllChildNodes(grid);
+    grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    for (let i = 0; i < val*val; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        grid.appendChild(div); 
+    }
 });
 
 
