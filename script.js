5447b7107cec2f854fe86044e9e86fd911c7207e
Daniel_J Carig <24857822+dannnnnn04@users.noreply.github.com>
2023-12-23 11:37:48 -0800

    Set hover effect so that grid divs change color
    -set up "hover" effect so that grid divs change color when mouse passes over them.
    -set up event listeners for each grid div to achieve this effect  (changing their background color using JavaScript)

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ createGrid = () => {
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.classList.add('cell');
+    div.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = 'black';
+    })
     grid.appendChild(div); 
 }
 };
